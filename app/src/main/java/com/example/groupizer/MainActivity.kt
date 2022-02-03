100d37254e29c5ce6acd06e5c7cce6e100f3a50e
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-02-04 01:41:57 +0200

    feat: Design Home Fragment feat: Design Login Fragment

--- app/src/main/java/com/example/groupizer/MainActivity.kt
+++ app/src/main/java/com/example/groupizer/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.groupizer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.groupizer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        setSupportActionBar(binding.mainToolbar)
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHost.navController
+
+        setupActionBarWithNavController(navController)
+
+
+        /*
+            A listener set for listening to changes in fragments current destination
+         */
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> {
+                    binding.mainToolbar.visibility = View.GONE
+                }
+                R.id.loginFragment -> {
+                    binding.mainToolbar.visibility = View.VISIBLE
+                }
+                R.id.registerFragment -> {
+                    binding.mainToolbar.visibility = View.VISIBLE
+                }
+                R.id.interestFragment -> {
+                    binding.mainToolbar.visibility = View.VISIBLE
+                }
+
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
